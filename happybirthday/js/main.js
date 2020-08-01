062afab63da4f78b4a723d6bfdf95280090e548b
nuphor <1812003189@qq.com>
2020-08-01 22:27:11 +0800

    生日贺卡完工

--- happybirthday/js/main.js
+++ happybirthday/js/main.js
@@ -1,7 +1,51 @@
 /*rem布局事件操作函数*/
-function rem(){
-    let docel=document.documentElement
-    docel.style.fontSize=docel.clientWidth / 5.8 +'px'
-}
-rem()
-window.addEventListener('resize',rem,false)
+
+
+
+/*jqajax-rem-isc-ready*/
+$(function(){
+    function rem(){
+        let docel=document.documentElement
+        docel.style.fontSize=docel.clientWidth / 5.8 +'px'
+    }
+    rem()
+    window.addEventListener('resize',rem,false)
+    let t=0,k=1000
+    // 背景
+    $('#card-background').fadeIn(k)
+    t+=k
+    // 相框
+    setTimeout(()=>{
+        $('#card-content-wrapper').fadeIn(k)
+    },t)
+    t+=k
+    // 马首
+    setTimeout(_=>{
+        $('#horse').fadeIn(500)
+    },t)
+    t+=500
+    // 贺词框
+    setTimeout(_=>{
+        $('#congratulations').fadeIn(500)
+    },t)
+    t+=500,k=1000
+    // 时间
+    setTimeout(_=>{
+        $('#date').fadeIn(k)
+        $('#mother').fadeOut(k)
+    },t)
+    t+=k
+    // 英文
+    setTimeout(_=>{
+        $('#in_english').fadeIn(500)
+    },t)
+    t+=500
+    // 中文
+    setTimeout(_=>{
+        $('#in_chinese').fadeIn(500)
+    },t)
+    t+=500
+    // $('#horse').animate({top:'0'},2000)
+
+    
+})//end jqajax-rem-isc-ready
